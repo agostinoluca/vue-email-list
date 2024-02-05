6c50166f316f78f95572713117226ab28a25943d
agostinoluca <agostino.luca89@gmail.com>
2024-02-05 20:51:37 +0100

    add for loop

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,14 @@ export default {
       .then((response) => {
         console.log(response.data.response);
       })
+
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+          console.log(response.data.response);
+        })
+    }
   }
 };
 </script>
