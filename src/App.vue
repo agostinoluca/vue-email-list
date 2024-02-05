6f899e487ba92183d46e190ef154feb0d5ca3b2d
agostinoluca <agostino.luca89@gmail.com>
2024-02-05 20:42:35 +0100

    add structure in template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,44 @@
-<script></script>
+<script>
+export default {
+  name: 'App',
+  data() {
 
-<template></template>
+    return {
+      name: 'Email List:',
+      emailList: ['example@1', 'example@2', 'example@3']
+    };
+  },
+  methods: {
 
-<style></style>
+  }
+};
+</script>
+
+<template>
+  <div class="container mt-5">
+    <div class="card p-3 w-75 m-auto">
+      <div class="d-flex justify-content-around align-items-center">
+        <h1 class="text-center">{{ name }}</h1>
+      </div>
+      <div class="d-flex justify-content-center ">
+        <ul class="list-group fs-3">
+          <li class="list-group-item d-flex justify-content-between" v-for="(email, index) in emailList" :key="index">
+            {{ email }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template >
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #ededed;
+}
+</style>
