79968e73582b81040508afb4c612bc3686acfe1b
agostinoluca <agostino.luca89@gmail.com>
2024-02-05 20:58:19 +0100

    add generate mails in page

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@ export default {
         .get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((response) => {
           console.log(response.data.response);
+          this.emailList.push(response.data.response);
         })
     }
   }
