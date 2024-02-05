d80ad2c40f0c103803404d2a1764561340f3caee
agostinoluca <agostino.luca89@gmail.com>
2024-02-05 20:48:10 +0100

    add console.log for take data mail

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
     axios
       .get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then((response) => {
-        console.log(response);
+        console.log(response.data.response);
       })
   }
 };
